baf6a03473e11f471d8a09aa6775c05a4c577ddb
Mario Rocha <111705910+mariorzk@users.noreply.github.com>
2024-09-09 11:42:30 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,26 @@
+var dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal, a 'Fadinha', é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e se tornou um símbolo da nova geração do skate.",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "fadinha skatista br rayssa show sk8 fenomeno br skatista top menina de ouro pódio certo talento puro skt profi"
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes do esporte brasileiro. A ginasta, com sua força, agilidade e determinação, conquistou o coração dos brasileiros e se tornou um ícone da ginástica mundial.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "ginasta top rebeca rainha power br rebeca show craque na ginástica força total sucesso na arena medalhista br show de solo ginástica profi"
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris. Com sua técnica impecável e determinação, ela conquistou a medalha de ouro e inspirou milhares de jovens atletas.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judoca br medalha ouro beatriz campeã luta braba ouro na mão br nas olimpiadas judô top força bruta paris 2024 inspiraçao no tatame"
+    },
+    {
+      titulo: "Ana Carolina Azevedo",
+      descricao: "Ana Carolina de Jesus Azevedo é uma velocista e atleta olímpica brasileira. Ela compete nas categorias 100m, 200m e 4x100m. Já foi campeã brasileira 5 vezes, participou dos Jogos Olímpicos de Tóquio 2020 e dos Jogos Sul-Americanos de 2022.",
+      link: "https://pt.wikipedia.org/wiki/Ana_Carolina_Azevedo",
+      tags: "velocista br ana corre muito olimpiadas tokyo sul-americano campea br 100m br 200m rápido ana top atleta top recordista pista"
+    }
+];
